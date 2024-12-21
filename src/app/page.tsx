009b173a9b31bddf925f441ebe843ac077e61ae2
ConnorBStill <connor@lamarsoftware.io>
2024-12-20 22:28:49 -0800

    Add force-dynamic; vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = 'force-dynamic';
+
 const urls = [
   'https://utfs.io/f/RhgEDhb3FCXDmzgWUsQIHJBQRNDT8PuUhFqbijE2AXafyoCv',
   'https://utfs.io/f/RhgEDhb3FCXDHyynWOwigUaRfSc3JOQVl9oC0jhWp1nKxd2k',
